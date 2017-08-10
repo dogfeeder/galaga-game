62ffcc3f0c933710a28bcd561d5f395c82d14c68
Patrick Freeman <trick05@bigpond.net.au>
2017-08-10 22:33:18 +1000

    Excalibur example up and running

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: 'wwwroot',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        loaders: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, include: /src/, loader: 'ts-loader' }
+        ]
+    }
+}
